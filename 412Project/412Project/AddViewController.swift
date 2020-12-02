3ccb06db7b80e578b7acb11ca7893da6fc55cc7a
Anthony Salaices <40293389+AnthonySalaices@users.noreply.github.com>
2020-12-01 17:16:01 -0800

    add view controller stuff

--- 412Project/412Project/AddViewController.swift
+++ 412Project/412Project/AddViewController.swift
@@ -21,44 +21,43 @@ class AddViewController: UIViewController {
             headerLbl.text = "Insert a State"
         }
         
-        //insertCountry()
-        insertState()
+        insertCountry()
+        //insertState()
         // Do any additional setup after loading the view.
     }
     
     func insertCountry(){
-        let json: [String: Any] = ["id": "HEEHAW",
-                                    "tests": 0,
-                                     "deaths": 0,
-                                      "casesperonemillion": 0,
-                                       "cases": 0,
-                                        "active": 0,
-                                         "critical": 0,
-                                          "todaydeaths": 0,
-                                           "deathsperonemillion": 0,
-                                            "todaycases": 0]
-
-        let jsonData = try? JSONSerialization.data(withJSONObject: json)
-
-        // create post request
-        let url = URL(string: "http://127.0.0.1:8080/addCountry")!
-        var request = URLRequest(url: url)
+        var id = "MM"
+        var cases = 0
+        var positiveincrease = 0
+        var negativeincrease = 0
+        var deaths = 0
+        var tests = 0
+        
+        let url = URL(string: "http://127.0.0.1:8080/addCountry/Mooo/0/0/0/0/0/0/0/0/0/0")
+        var request = URLRequest(url: url!)
         request.httpMethod = "POST"
-
-        // insert json data to the request
-        request.httpBody = jsonData
 
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {
-                print(error?.localizedDescription ?? "No data")
+                // check for fundamental networking error
+                print("error=\(String(describing: error))")
                 return
             }
-            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-            if let responseJSON = responseJSON as? [String: Any] {
-                print(responseJSON)
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200
+            {
+                // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
             }
+            
+            // print(data)
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "RefreshStatus"), object: nil)
         }
-
         task.resume()
     }
     
@@ -67,28 +66,12 @@ class AddViewController: UIViewController {
         var cases = 0
         var positiveincrease = 0
         var negativeincrease = 0
-        var deaths: Optional<Int> = Int("0")
-        var tests: Optional<Int> = Int("0")
+        var deaths = 0
+        var tests = 0
         
-        
-        let parameters = "{\"id\":\"\(String(describing: id))\"," +
-            "\"cases\":\"\(String(describing: cases))\"," +
-            "\"positiveincrease\":\"\(String(describing: positiveincrease))\"," +
-            "\"negativeincrease\":\"\(String(describing: negativeincrease))\","  +
-            "\"deaths\":\"\(String(describing: deaths))\"," +
-                "\"tests\":\"\(String(describing: tests))\"}"
-        
-        
-        print(parameters)
-        
-        let url = URL(string: "http://127.0.0.1:8080/addState")
+        let url = URL(string: "http://127.0.0.1:8080/addState/MM/0/0/0/0/0")
         var request = URLRequest(url: url!)
         request.httpMethod = "POST"
-        let postParameters = parameters
-        request.httpBody = postParameters.data(using: .utf8)
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        request.setValue("application/json;charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        request.addValue("*", forHTTPHeaderField: "Access-Control-Allow-Origin")
 
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, error == nil else {
